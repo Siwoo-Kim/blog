723d7cf52caad1a631b173d7d8469165ab4e8d5a
Siwoo-Kim <skim327@myseneca.ca>
2018-01-21 21:35:11 -0500

    version 2-10

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -3,25 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import {HttpModule} from "@angular/http";
-import {FormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatButtonModule, MatCardModule, MatInputModule, MatRadioModule, MatToolbarModule, MatSnackBarModule} from "@angular/material";
+import {MatButtonModule, MatCardModule, MatSnackBarModule} from "@angular/material";
+import {HttpModule} from "@angular/http";
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { MainDashboardComponent } from './components/main-dashboard/main-dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent,
+    MainDashboardComponent
   ],
   imports: [
-    /* Angular Module */
-    BrowserModule,HttpModule,FormsModule,
-
-    /* Animation and Material Module */
+    BrowserModule,
+    HttpModule,
     BrowserAnimationsModule,
-    MatToolbarModule, MatSnackBarModule, MatCardModule,
-    MatInputModule, MatRadioModule, MatButtonModule,
-    /* App Module */,
+    MatSnackBarModule,MatCardModule,MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
